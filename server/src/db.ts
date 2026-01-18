cb4015a4b24e16e7c5754fee931fa09f78798a57
Vukp17 <vuk.papic@student.um.si>
2026-01-18 21:16:19 +0100

    Removed legacy server

--- server/src/db.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-import { env } from "./config/env.js";
-
-export async function connectToDatabase(): Promise<void> {
-  if (mongoose.connection.readyState === 1) return;
-  await mongoose.connect(env.mongoUri);
-}
-
